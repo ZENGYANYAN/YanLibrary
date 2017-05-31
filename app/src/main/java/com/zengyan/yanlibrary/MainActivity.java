802dbf6be83295c69cb8951283356bd4771a6259
ZengYan <810989881@qq.com>
2017-05-31 09:31:44 +0800

    加入viewholder

--- app/src/main/java/com/zengyan/yanlibrary/MainActivity.java
+++ app/src/main/java/com/zengyan/yanlibrary/MainActivity.java
@@ -1,13 +1,49 @@
 package com.zengyan.yanlibrary;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.zengyan.yanbottomdialog.ViewHolder;
+import com.zengyan.yanbottomdialog.YBottomDialog;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.R.string.no;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @BindView(R.id.normal_dialog)
+    Button normalBtn;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        normalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                YBottomDialog dialog=new YBottomDialog(MainActivity.this,R.layout.normal_dialog) {
+                    @Override
+                    protected void initViews(ViewHolder viewHolder) {
+                        viewHolder.setText(R.id.text, "aaaaaaaa");
+                    }
+                };
+                dialog.show();
+            }
+        });
+
+
     }
 }
---
+++ yanbottomdialog/src/main/java/com/zengyan/yanbottomdialog/YBottomDialog.java
@@ -0,0 +1,73 @@
+package com.zengyan.yanbottomdialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.StyleableRes;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+/**
+ * Created by ZengYan on 2017/5/8.
+ * Email : 810989881@qq.com
+ */
+
+public abstract class YBottomDialog extends Dialog {
+
+    protected View rootView;
+    private FrameLayout childView;
+    private Context mContext;
+    private int mLayoutId;
+
+
+    public YBottomDialog(@NonNull Context context, final int layoutId) {
+        super(context, R.style.YBottomDialog);
+        this.mContext = context;
+        this.mLayoutId = layoutId;
+    }
+
+    protected abstract void initViews(ViewHolder viewHolder);
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ViewTreeObserver observer = childView.getViewTreeObserver();
+        ViewTreeObserver.OnGlobalLayoutListener listener = new ViewTreeObserver.OnGlobalLayoutListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onGlobalLayout() {
+                childView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                Window dialogWindow = getWindow();
+                WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+                int height = childView.getHeight();
+                lp.height = height;
+                dialogWindow.setAttributes(lp);
+                dialogWindow.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM);
+            }
+        };
+
+        observer.addOnGlobalLayoutListener(listener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        rootView = getLayoutInflater().inflate(R.layout.y_rootview_dialog_layout, null);
+        childView = (FrameLayout) rootView.findViewById(R.id.content_view);
+        View contentView = getLayoutInflater().inflate(mLayoutId, null);
+        childView.addView(contentView);
+        ViewHolder holder = new ViewHolder(contentView);
+        initViews(holder);
+        getWindow().setContentView(rootView);
+        this.setCanceledOnTouchOutside(false);
+    }
+
+}
